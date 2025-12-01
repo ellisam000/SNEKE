a0966ab600f3774b9e647184b545fc251678242c
Amya Ellis <ellisam000@warren.k12.in.us>
2025-12-01 08:18:38 -0500

    DONE

---
+++ day04/snake.py
@@ -0,0 +1,104 @@
+import pygame, random
+
+# Initialize pygame
+pygame.init()
+
+# Set display window
+WINDOW_WIDTH = 600
+WINDOW_HEIGHT = 600
+display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+pygame.display.set_caption("~~SNEKE~~")
+
+# Set FSP and clock
+FPS = 20
+clock = pygame.time.Clock()
+# Set game values
+SNAKE_SIZE = 20
+head_x = WINDOW_WIDTH // 2
+head_y = WINDOW_HEIGHT // 2 + 100
+snake_dx = 0
+snake_dy = 0
+score = 0
+# Set colors
+GREEN = (0, 255, 0)
+DARKGREEN = (10, 50, 10)
+RED = (255, 0, 0)
+DARKRED = (150, 0, 0)
+WHITE = (255, 255, 255)
+# Set fonts
+font = pygame.font.SysFont('gabriola', 48)
+# Set text
+title_text = font.render("~~Snake~~", True, GREEN, DARKRED)
+title_rect = title_text.get_rect()
+title_rect.center = (WINDOW_WIDTH // 2, WINDOW_HEIGHT // 2)
+
+score_text = font.render("Score: 0", True, GREEN, DARKRED)
+score_rect = score_text.get_rect()
+score_rect.topleft = (10,10)
+
+game_over_text =font.render("GAMEOVER", True, RED, DARKRED)
+game_over_rect = game_over_text.get_rect()
+game_over_rect.center = (WINDOW_WIDTH // 2, WINDOW_HEIGHT // 2)
+
+continue_text = font.render("Press any key to play again", True, RED, DARKGREEN)
+continue_rect = continue_text.get_rect()
+continue_rect.center = (WINDOW_WIDTH // 2, WINDOW_HEIGHT // 2 + 64)
+
+# Set sounds and music
+pick_up_sound = pygame.mixer.Sound("pick_up_sound.wav")
+# Set images (in this case, use simple rects...so just create their coordinates)
+# For a rectangle you need (top-left x, top-left y, width, height)
+apple_coord = (500, 500, SNAKE_SIZE, SNAKE_SIZE)
+apple_rect = pygame.draw.rect(display_surface, RED, apple_coord)
+
+head_coord = head_x, head_y, SNAKE_SIZE, SNAKE_SIZE
+head_rect = pygame.draw.rect(display_surface, GREEN, head_coord)
+
+# The main game loop
+running = True
+while running:
+    # Check to see if the user wants to quit
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+        # Move the snake
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                snake_dx = -1 * SNAKE_SIZE
+                snake_dy = 0
+        if event.key == pygame.K_RIGHT:
+            snake_dx = SNAKE_SIZE
+            snake_dy = 0 * SNAKE_SIZE
+
+        if event.key == pygame.K_UP:
+            snake_dx = 0
+            snake_dy = -1 * SNAKE_SIZE
+
+        if event.key == pygame.K_DOWN:
+            snake_dx = 0
+            snake_dy = SNAKE_SIZE
+
+
+    # Add the head coordinate to the first index of the body coordinate list
+    # This will essentilalyl move all of the snakes body by one position in the list
+body_coords.
+head_coords = 0
+    # Update the x,y position of the snakes head and make a new coordinate
+
+    # Check for game over
+
+    # Check for collisions
+
+    # Update HUD
+
+    # Fill the surface
+
+    # Blit HUD
+
+    # Blit assets
+
+    # Update display and tick clock
+
+# End the game
+pygame.quit()
--- day05/snake_notes.py
+++ day05/snake_notes.py
@@ -11,26 +11,14 @@ display_surface = pygame.display.set_mode(size)
 pygame.display.set_caption("~~SNEKE~~")
 
 # Set FSP and clock
-# TODO: make a variable (constant) called FPS and initialize to 20
+#
 clock = pygame.time.Clock()
 
 # Set game values
-# TODO: make a variable (constant) named SNAKE_SIZE and initialize to 20
-
-# TODO: make a variable named head_x and assign half of the WINDOW_WIDTH to it.  use integer division //  (i.e. 11 / 2 is 5.5,  11//2 is 5)
-# TODO: make a variable named head_y and assign half of the WINDOW_HEIGHT + 100 to it.  use integer division //
-
-# TODO: make a variable named snake_dx and assign 0 to it.
-# TODO: repeat for a variable named snake_dy
-
-# TODO: make a variable named score and assign 0 to it.
+#
 
 # Set colors
-GREEN = (0, 255, 0)  # (r, g, b)
-# TODO: make a DARKGREEN color with rgb(10, 50, 10)
-# TODO: make a RED
-DARKRED = (150, 0, 0)
-# TODO: make a WHITE
+
 
 # Set fonts
 font = pygame.font.SysFont('gabriola', 48)
@@ -41,17 +29,7 @@ title_rect = title_text.get_rect()  # gets the box containing the text object
 title_rect.center = (WINDOW_WIDTH // 2,
                      WINDOW_HEIGHT // 2)  # places the box containing the text object's center to the middle of the screen.
 
-# TODO: make a score_text object and assign a font render to it with text "Score: 0", antialias of True, color of GREEN, background color of DARKRED
-# TODO: make a score_rect object by assigning score_text.get_rect() to it.
-# TODO: place the topleft of score_rect to an x coordinate of 10 and y coordinate of 10
-
-# TODO: make a game_over_text object and assign a font render to it with text "GAMEOVER", antialias of True, color of RED, background color of DARKRED
-# TODO: make a game_over_rect object by assigning game_over_text.get_rect() to it.
-# TODO: place the center of game_over_rect to an x coordinate of half the WINDOW_WIDTH and y coordinate of half the WINDOW_HEIGHT
-
-# TODO: make a continue_text  object and assign a font render to it with text "Press any key to play again", antialias of True, color of RED, background color of DARKGREEN
-# TODO: make a continue_rect  object by assigning continue_text.get_rect() to it.
-# TODO: place the center of continue_rect  to an x coordinate of half the WINDOW_WIDTH and y coordinate of half the WINDOW_HEIGHT + 64
+#
 
 # Set sounds and music
 pick_up_sound = pygame.mixer.Sound("pickup_sound.wav")
@@ -61,8 +39,6 @@ pick_up_sound = pygame.mixer.Sound("pickup_sound.wav")
 apple_coord = (500, 500, SNAKE_SIZE, SNAKE_SIZE)
 apple_rect = pygame.draw.rect(display_surface, RED, apple_coord)
 
-# TODO: make a tuple for the head coordinates named head_coord and set to head_x, head_y, SNAKE_SIZE, SNAKE_SIZE
-# TODO: make head_rect in a way similar to apple_rect, but with color GREEN instead.
 
 body_coords = []
 
@@ -76,25 +52,7 @@ while running:
             running = False
 
         # Move the snake
-        # TODO: check if the event.type is pygame.KEYDOWN
-            # Inside the block of the if statement where we checked if the eventy type is KEYDOWN
-            # TODO: check if the event.key is pygame.K_LEFT.
-                # Inside the block of the K_LEFT if statement
-                # TODO: assign -1 * SNAKE_SIZE 'TO' snake_dx.  This moves the snake 1 SNAKE_SIZE to the left.
-                # TODO: assign 0 'TO' snake_dy.  We may have been moving up or down.  This cancels that movement out.
-            # TODO: check if the event.key is pygame.K_RIGHT.
-                # Inside the block of the K_RIGHT if statement.
-                # TODO: assign SNAKE_SIZE 'TO' snake_dx.  This moves the snake 1 SNAKE_SIZE to the right.
-                # TODO: assign 0 'TO' snake_dy.  We may have been moving up or down.  This cancels that movement out.
-            # TODO: check if the event.key is pygame.K_UP.
-                # Inside the block of the K_UP if statement.
-                # TODO: assign 0 'TO' snake_dx.  We may have been moving left or right.  This cancels that movement out.
-                # TODO: assign -1 * SNAKE_SIZE 'TO' snake_dx. This moves the snake 1 SNAKE_SIZE up.
-            # TODO: check if the event.key is pygame.K_DOWN.
-                # Inside the block of the K_DOWN if statement.
-                # TODO: assign 0 'TO' snake_dx.  We may have been moving left or right.  This cancels that movement out.
-                # TODO: assign SNAKE_SIZE 'TO' snake_dx. This moves the snake 1 SNAKE_SIZE down.
-
+        #
 
     # Add the head coordinate to the first index of the body coordinate list
     # This will essentially move all the snakes body by one position in the list
@@ -110,7 +68,7 @@ while running:
     if head_rect.left < 0 or head_rect.right > WINDOW_WIDTH or head_rect.top < 0 or head_rect.bottom > WINDOW_HEIGHT or head_coord in body_coords:
         # TODO: call display_surface's blit function passing in game_over_text and game_over_rect
         # TODO: repeat for continue
-        # TODO: call pygame.display_update()
+        # TODO: call pygame.display.update()
 
         #Pause the game until the player presses a key, then reset the game
         # TODO: assign True to is_paused
@@ -143,7 +101,7 @@ while running:
         # TODO: assign (apple_x, apple_y, SNAKE_SIZE, SNAKE_SIZE) to apple_coord
 
         # TODO: call body_coords' append method and pass in head_coord for it's argument
-        pass # TODO: remove this line.
+
 
 
     # Update HUD
@@ -190,6 +148,7 @@ while running:
     1. "Call pygame.draw.rect and passing in display_surface, GREEN, and head_coord for the head of the snake
     2. "Call pygame.draw.rct again pass in display_surface, RED, and apple_coord for the apple.  
     '''
+
 
     # Update display and tick clock
     # TODO: Read below. 2 TODO's, require's explanation
